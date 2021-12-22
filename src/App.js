665925540d27b4b4ffdeb646887efcc14e8fcd10
jinwon jeong <jw@imrbiz.co.kr>
2021-12-22 15:03:40 +0900

    commit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
-const App = () => <div>App</div>;
+import styled from "styled-components";
 
+class App extends React.Component {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+      to right,
+      rgba(20, 20, 20, 0.1) 10%,
+      rgba(20, 20, 20, 0.7) 70%,
+      rgba(20, 20, 20, 1)
+    ),
+    url(https://source.unsplash.com/random/1920x1080);
+  background-size: cover;
+`;
 export default App;
-
